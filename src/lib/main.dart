9a40004645cb3639b2b1e5c88cee45dc7c5667f1
Patria Anggara Susilo Putra <susiloangga17@gmail.com>
2023-10-27 14:33:45 +0700

    splash screen - beta 1

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:src/splash_screen/splash_screen.dart';
+import 'home_page.dart';
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  static const String homePageRoute = '/home_page';
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Aster Retsa Car Rental',
-      home: Scaffold(
-        body: SafeArea(
-          child: Container(
-            child: const Text("Hello World"),
-          ),
-        ),
-      ),
+      home: const SplashScreen(),
+      routes: {
+        homePageRoute: (context) => HomePage(),
+      },
     );
   }
 }
---
+++ src/lib/splash_screen/splash_screen.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:src/main.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+     Future.delayed(const Duration(seconds: 5), () {
+        setState(() {
+          _isLoading = false;
+        });
+     });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF222525),
+      body: SafeArea(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Image(
+              image: AssetImage("assets/images/text_logo.png"),
+              fit: BoxFit.cover,
+              isAntiAlias: true,
+              width: 267,
+              height: 115,
+            ),
+            const Image(
+              image: AssetImage("assets/images/logo.png"),
+              fit: BoxFit.cover,
+              isAntiAlias: true,
+              width: 375,
+              height: 375,
+            ),
+             RichText(
+              text: const TextSpan(
+                children: [
+                  TextSpan(
+                    text: 'Dream ',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold, // Mengatur tebal untuk "Dream"
+                      fontSize: 15,
+                    ),
+                  ),
+                  TextSpan(
+                    text: 'Cars, ',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                    ),
+                  ),
+                  TextSpan(
+                    text: 'Real ',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold, // Mengatur tebal untuk "Real"
+                      fontSize: 15,
+                    ),
+                  ),
+                  TextSpan(
+                    text: 'Journeys',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            _isLoading && _firstTime ? CircularProgressIndicator(
+              color: Colors.white,
+            ) : ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(345, 50),
+                backgroundColor: Color(0xFFFFFFFF),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                ),
+              ),
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, MyApp.homePageRoute);
+              },
+              child: Text(
+                "Let's Go",
+                style: GoogleFonts.poppins(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
