07703f3508f0f4cd38e8f21a2892051ce2f83045
Patria Anggara Susilo Putra <susiloangga17@gmail.com>
2023-11-28 23:24:22 +0700

    fix

--- src/lib/pages/splash_screen.dart
+++ src/lib/pages/splash_screen.dart
@@ -22,116 +22,120 @@ class SplashScreen extends StatelessWidget {
         child: BlocBuilder<SplashBloc, SplashState>(
           bloc: splashBloc,
           builder: (context, state) {
-            if (state is SplashLoading) {
-              return Column(
-                children: [
-                  _buildSplashWidget(),
-                  LoadingAnimationWidget.stretchedDots(
-                      color: Colors.white,
-                      size: 100,
-                  ),
-                ],
-              );
-            } else if (state is SplashLoaded) {
-              return ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(345, 50),
-                    backgroundColor: const Color(0xFFFFFFFF),
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(20),
-                      ),
-                    ),
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      PageTransition(
-                          ctx: context,
-                          child: const LoginPage(),
-                          inheritTheme: true,
-                          alignment: Alignment.bottomCenter,
-                          duration: const Duration(milliseconds: 500),
-                          type: PageTransitionType.rightToLeft),
-                    );
-                  },
-                  child: Text(
-                    "Let's Go",
-                    style: GoogleFonts.poppins(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 15,
-                    ),
-                  ),
-                );
-            } else {
-              return SizedBox();
-            }
+            return _buildSplashWidget(context, state);
           },
         ),
       ),
     );
   }
 
-  Widget _buildSplashWidget() {
+  Widget _buildSplashWidget(BuildContext context, SplashState state) {
     return Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const Image(
+          image: AssetImage("assets/images/splash_screen/text_logo.png"),
+          fit: BoxFit.cover,
+          isAntiAlias: true,
+          width: 267,
+          height: 115,
+        ),
+        const Image(
+          image: AssetImage("assets/images/splash_screen/logo.png"),
+          fit: BoxFit.cover,
+          isAntiAlias: true,
+          width: 375,
+          height: 375,
+        ),
+        RichText(
+          text: TextSpan(
             children: [
-              const Image(
-                image: AssetImage("assets/images/splash_screen/text_logo.png"),
-                fit: BoxFit.cover,
-                isAntiAlias: true,
-                width: 267,
-                height: 115,
-              ),
-              const Image(
-                image: AssetImage("assets/images/splash_screen/logo.png"),
-                fit: BoxFit.cover,
-                isAntiAlias: true,
-                width: 375,
-                height: 375,
-              ),
-              RichText(
-                text: TextSpan(
-                  children: [
-                    TextSpan(
-                      text: 'Dream ',
-                      style: GoogleFonts.poppins(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                      ),
-                    ),
-                    TextSpan(
-                      text: 'Cars, ',
-                      style: GoogleFonts.poppins(
-                        color: Colors.white,
-                        fontWeight: FontWeight.normal,
-                        fontSize: 20,
-                      ),
-                    ),
-                    TextSpan(
-                      text: 'Real ',
-                      style: GoogleFonts.poppins(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                      ),
-                    ),
-                    TextSpan(
-                      text: 'Journeys',
-                      style: GoogleFonts.poppins(
-                        color: Colors.white,
-                        fontWeight: FontWeight.normal,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ],
+              TextSpan(
+                text: 'Dream ',
+                style: GoogleFonts.poppins(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
                 ),
               ),
+              TextSpan(
+                text: 'Cars, ',
+                style: GoogleFonts.poppins(
+                  color: Colors.white,
+                  fontWeight: FontWeight.normal,
+                  fontSize: 20,
+                ),
+              ),
+              TextSpan(
+                text: 'Real ',
+                style: GoogleFonts.poppins(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              TextSpan(
+                text: 'Journeys',
+                style: GoogleFonts.poppins(
+                  color: Colors.white,
+                  fontWeight: FontWeight.normal,
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
+        _buildButtonAction(context, state),
       ],
+    );
+  }
+
+  Widget _buildButtonAction(BuildContext context, SplashState state) {
+    if (state is SplashLoading) {
+      return LoadingAnimationWidget.stretchedDots(
+        color: Colors.white,
+        size: 100,
+      );
+    } else if (state is SplashLoaded) {
+      return _buildButtonWidget(context);
+    } else {
+      return const SizedBox();
+    }
+  }
+
+  Widget _buildButtonWidget(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        minimumSize: const Size(345, 50),
+        backgroundColor: const Color(0xFFFFFFFF),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(20),
+          ),
+        ),
+      ),
+      onPressed: () {
+        Navigator.push(
+          context,
+          PageTransition(
+              ctx: context,
+              child: const LoginPage(),
+              inheritTheme: true,
+              alignment: Alignment.bottomCenter,
+              duration: const Duration(milliseconds: 500),
+              type: PageTransitionType.rightToLeft),
+        );
+        },
+      child: Text(
+        "Let's Go",
+        style: GoogleFonts.poppins(
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+          fontSize: 15,
+        ),
+      ),
     );
   }
 }
