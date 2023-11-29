99ce5cb4517ae17cf6321901fc4844c921d0910f
Patria Anggara Susilo Putra <susiloangga17@gmail.com>
2023-11-29 10:02:38 +0700

    setting class SplashScreenState

---
+++ src/lib/bloc/splash_screen_bloc/splash_screen_state.dart
@@ -0,0 +1,7 @@
+abstract class SplashScreenState {}
+
+class InitialSplashScreenState extends SplashScreenState {}
+
+class LoadingSplashScreenState extends SplashScreenState {}
+
+class LoadedSplashScreenState extends SplashScreenState {}
