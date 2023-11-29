99b29c44f16985916a141c9e865ed2db40e56b1d
Patria Anggara Susilo Putra <susiloangga17@gmail.com>
2023-11-29 10:21:27 +0700

    menambahkan bloc builder untuk menampilkan LoadingAnimationWidget saat masih delay dan menampilkan widget ElevatedButton saat delay telah berakhir

--- src/lib/pages/splash_screen.dart
+++ src/lib/pages/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:page_transition/page_transition.dart';
@@ -87,41 +88,50 @@ class _SplashScreenState extends State<SplashScreen> {
                   ],
                 ),
               ),
-              isLoading
-            ? LoadingAnimationWidget.stretchedDots(
-                color: Colors.white,
-                size: 100,
-              )
-            : ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size(345, 50),
-                  backgroundColor: const Color(0xFFFFFFFF),
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(20),
-                    ),
-                  ),
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    PageTransition(
-                        ctx: context,
-                        child: const LoginPage(),
-                        inheritTheme: true,
-                        alignment: Alignment.bottomCenter,
-                        duration: const Duration(milliseconds: 500),
-                        type: PageTransitionType.rightToLeft),
-                  );
+              BlocBuilder<SplashScreenBloc, SplashScreenState>(
+                bloc: _splashScreenBloc,
+                builder: (context, state) {
+                  if (state is LoadingSplashScreenState) {
+                    return LoadingAnimationWidget.stretchedDots(
+                      color: Colors.white,
+                      size: 100,
+                    );
+                  } else if (state is LoadedSplashScreenState) {
+                    return ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: const Size(345, 50),
+                        backgroundColor: const Color(0xFFFFFFFF),
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(20),
+                          ),
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          PageTransition(
+                              ctx: context,
+                              child: const LoginPage(),
+                              inheritTheme: true,
+                              alignment: Alignment.bottomCenter,
+                              duration: const Duration(milliseconds: 500),
+                              type: PageTransitionType.rightToLeft),
+                        );
+                      },
+                      child: Text(
+                        "Let's Go",
+                        style: GoogleFonts.poppins(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                        ),
+                      ),
+                    );
+                  } else {
+                    return const SizedBox();
+                  }
                 },
-                child: Text(
-                  "Let's Go",
-                  style: GoogleFonts.poppins(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15,
-                  ),
-                ),
               ),
             ],
           ),
