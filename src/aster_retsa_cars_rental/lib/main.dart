b29f82c9fd2dc387097757fe3b1ba70d3bce5f41
Patria Anggara Susilo Putra <susiloangga17@gmail.com>
2023-12-05 00:34:01 +0700

    konekni ke firebase menggunakan CLI, menambahkan dependencies firebase_core, firebase_auth, firebase_storage, cloud_firestore dan inisialisasi firebase

--- src/aster_retsa_cars_rental/lib/main.dart
+++ src/aster_retsa_cars_rental/lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:aster_retsa_cars_rental/firebase_options.dart';
 import 'package:aster_retsa_cars_rental/pages/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
