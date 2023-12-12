b8a4df6ce6984c0dabec52cc98665e8d930ed3e4
Patria Anggara Susilo Putra <susiloangga17@gmail.com>
2023-12-12 19:02:39 +0700

    menmbahkan factory method pada clas brand

--- src/aster_retsa_cars_rental/lib/models/brand.dart
+++ src/aster_retsa_cars_rental/lib/models/brand.dart
@@ -1,4 +1,8 @@
 class Brand {
   String image;
   Brand({required this.image});
+
+  factory Brand.fromMap(Map<String, dynamic> data) {
+    return Brand(image: data['image'] ?? "Gagal mengambil gambar");
+  }
 }
