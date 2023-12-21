bf1525e7a3b8650cc151d2e6b9528890dec2a51f
Bahtiar Rifai <bahtiarderifai@gmail.com>
2023-12-21 15:16:40 +0700

    salah merge

--- src/aster_retsa_cars_rental/lib/pages/register_page.dart
+++ src/aster_retsa_cars_rental/lib/pages/register_page.dart
@@ -39,14 +39,13 @@ class _RegisterPageState extends State<RegisterPage> {
         email: emailController.text,
       );
       Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => HomePage()),
-        );
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
     }
   }
 
-
-  void _handleGoogleSignIn()  {
+  void _handleGoogleSignIn() {
     try {
       GoogleAuthProvider googleAuthProvider = GoogleAuthProvider();
       _auth.signInWithProvider(googleAuthProvider);
@@ -66,13 +65,11 @@ class _RegisterPageState extends State<RegisterPage> {
   void initState() {
     super.initState();
     _auth.authStateChanges().listen((event) {
-    _auth.authStateChanges().listen((event) {
       setState(() {
         _user = event;
       });
     });
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -121,7 +118,6 @@ class _RegisterPageState extends State<RegisterPage> {
                         controller: passwordController,
                         hintText: 'Password',
                         obsecure: true),
-                        obsecure: true),
                   ],
                 ),
               ),
@@ -129,7 +125,6 @@ class _RegisterPageState extends State<RegisterPage> {
               Container(
                 margin: const EdgeInsets.only(left: 20, right: 20),
                 child: ButtonFront(
-                  onTap: () => registerUser(context),
                   onTap: () => registerUser(context),
                   theText: 'Register',
                   textColor: const Color.fromARGB(255, 34, 37, 37),
